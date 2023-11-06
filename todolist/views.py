41dc01d4e881034cbd21f56d1376facc283f5539
miriamremesal <miriamremesal@gmail.com>
2023-11-06 14:18:46 +0100

    project finished

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Task 
 
-# Create your views here.
+def task_list(request):
+    task = Task.objects.all()
+    return render(request, 'todolist/task_list.html', {'task': task})
